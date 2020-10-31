69c3ba5fa3068d5d8f1e3e88b059d52fcf80cd43
Ewelina <piotrowska.ewelina@gmail.com>
2020-10-31 14:28:55 +0100

    ver 2.0

--- pages/cart_summary.py
+++ pages/cart_summary.py
@@ -9,31 +9,12 @@ class CartSummaryPage(BasePage):
     total_product = (By.ID, 'total_product')
     total_shipping = (By.ID, 'total_shipping')
 
-    def get_all_elements(self):
-        products_elements = self.driver.find_elements(*self.product_description_selector)
-        products_name = ""
-        for product in products_elements:
-            products_name += product.text
-        return products_name
-
     def check_the_price_smaller_50(self):
-        price_number = self.driver.find_elements(*self.total_product)
-        price = "$50.00"
-        price_1 = price.strip("$")
-        for price in price_number:
-            price_number += price.text
-            return price_1
-
-        if price_1 == "50.00":
-            return True
+        price_number = self.driver.find_element(*self.total_product).text
+        assert float(price_number.strip('$')) < 50
+        print("price is smaller than 50")
 
     def check_the_total_shipping_2(self):
-        price_total_shipping = self.driver.find_elements(*self.total_shipping)
-        shipping_price = "$2.00"
-        price_2 = shipping_price.strip("$")
-        for shipping_price in price_total_shipping:
-            price_total_shipping += shipping_price.text
-            return price_2
-
-        if price_2 == "2.00":
-            return True
+        price_total_shipping = self.driver.find_element(*self.total_shipping).text
+        assert float(price_total_shipping.strip('$')) == 2
+        print("shipping price is equal 2 dollars")
--- pages/products.py
+++ pages/products.py
@@ -13,6 +13,7 @@ class ProductsPage(BasePage):
     shopping_cart_selector = (By.XPATH, "//*[@class='shopping_cart']/a")
 
     def add_element_to_cart(self):
+        WebDriverWait(self.driver, 2).until(EC.element_to_be_clickable(self.product_container_selector))
         element = self.driver.find_element(*self.product_container_selector)
         hover = ActionChains(self.driver).move_to_element(element)
         hover.perform()
@@ -20,5 +21,5 @@ class ProductsPage(BasePage):
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.continue_shopping_selector)).click()
 
     def proceed_to_checkout(self):
-        WebDriverWait(self.driver, 13).until(EC.element_to_be_clickable(self.shopping_cart_selector)).click()
+        WebDriverWait(self.driver, 11).until(EC.element_to_be_clickable(self.shopping_cart_selector)).click()
 
